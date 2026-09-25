d65e421996a021184ed5fc6df05ac63bc740bb76
Muhammad Farrukh Jadoon <66329847+farrukhjadoon900@users.noreply.github.com>
2026-09-24 23:04:02 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
-
+const HERMES_CONFIG = {
+  REPO: "MFJ_TRADING/FL-200-Private",
+  BRANCH: "main",
+  PASSWORD: null,          // Secret se hash aayega
+  PRIVATE_MODE: true,      // 2-repo setup mein hamesha true
+  SESSION_DAYS: 7
+};
